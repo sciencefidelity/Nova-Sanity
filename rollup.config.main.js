e08d9a1d692f1000a9e05d00aae115cb7681c50c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-21 23:58:45 +0000

    refactor: change case of extension

--- rollup.config.main.js
+++ rollup.config.main.js
@@ -6,7 +6,7 @@ export default {
   input: "src/main.ts",
   plugins: [typescript(), commonjs(), resolve()],
   output: {
-    file: "Sanity.novaextension/Scripts/main.dist.js",
+    file: "sanity.novaextension/Scripts/main.dist.js",
     sourcemap: true,
     format: "cjs"
   }
---
+++ sanity.novaextension/README.md
@@ -0,0 +1,3 @@
+# Sanity
+
+Extension for [Nova](https://nova.app) that makes developing applications for [Sanity.io](https://sanity.io) that much more awesome.
